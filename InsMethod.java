59bacecdaea6f42da871e8ce95263a158ad6714a
Dthanit <43135317+Dthanit@users.noreply.github.com>
2018-10-25 15:37:22 +0700

    Update InsMethod.java

--- InsMethod.java
+++ InsMethod.java
@@ -7,14 +7,17 @@ class InstMethod
     for(int i=1;i<=3;i++)
     System.out.println(n);
   }
-  
-  public static void main(String[] args)
-  { 
+  public static void getInteger()
+  {
     Scanner sc = new Scanner(System.in);
-    getName();
     System.out.println();
     System.out.print("Enter Integer  ");
     int num = sc.nextInt();
     System.out.println("Integer is "+num);
+  }
+  public static void main(String[] args)
+  { 
+    getName();
+    getInteger();
   }
 }
